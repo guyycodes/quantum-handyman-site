5a11aae86345683a59e11053839bf243151d339b
guymorganb <guymorganb@gmail.com>
2025-09-21 21:58:20 -0600

    added the job creation and tracking

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
 import BookingCTA from '../Components/BookingCTA'
 import { useIntersectionObserver, useStaggeredIntersection } from '../hooks/useIntersectionObserver'
 import { 
